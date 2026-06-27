3414abd32233079e3717827ae77c8f1ca1b00c4a
1620 <adam@adamziel.com>
2026-06-27 19:07:22 +0000

    fix: preserve native definition terms (plib-wcz75)
    Executed-By: port_libs/polecats/1620

--- lanes/pandoc/src/NativeWriter.php
+++ lanes/pandoc/src/NativeWriter.php
@@ -778,6 +778,7 @@ final class NativeWriter
             'paragraph',
             'heading',
             'term',
+            'definition_term',
             'line',
             'emph',
             'strong',
@@ -1072,7 +1073,7 @@ final class NativeWriter
             $term = null;
             $definitions = [];
             foreach ($item->children as $child) {
-                if ($child->type === 'term') {
+                if (in_array($child->type, ['term', 'definition_term'], true)) {
                     $term = $child;
                 } elseif ($child->type === 'definition') {
                     $definitions[] = $this->renderBlockList($child->children, 0);
