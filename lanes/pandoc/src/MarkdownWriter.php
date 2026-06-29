fea8558099f89973c7737088096a9647b92f5e81
Adam Zieliński <adam@adamziel.com>
2026-06-29 01:00:26 +0000

    fix: handle markdown escaped delimiters (plib-vbm3m)
    Executed-By: port_libs/polecats/1782
    (cherry picked from commit 8197273479d95ff35dc52c47775e1d1c913744d4)

--- lanes/pandoc/src/MarkdownWriter.php
+++ lanes/pandoc/src/MarkdownWriter.php
@@ -8201,7 +8201,7 @@ final class MarkdownWriter
             }
 
             if (str_starts_with($tail, '~~')) {
-                $escaped .= '\\~~';
+                $escaped .= '\\~\\~';
                 $i++;
                 continue;
             }
