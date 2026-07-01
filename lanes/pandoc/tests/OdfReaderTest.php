45cc2918b39367d6569cae86cf3f654f6fa72d41
Adam Zieliński <adam@adamziel.com>
2026-07-01 22:16:46 +0000

    Bundle package provenance rollups for main
    Supersedes plib-wisp-40jch / plib-e70g4, plib-wisp-w47u3 / plib-chu3j, and plib-wisp-z6vj / plib-83qkt.
    
    Includes DOCX relationship path byte-length buckets, shared ZIP source-record review byte accounting, and ODF ZIP package-manifest entry-comment rollups.

--- lanes/pandoc/tests/OdfReaderTest.php
+++ lanes/pandoc/tests/OdfReaderTest.php
@@ -12918,6 +12918,20 @@ XML;
         $t->same(true, $provenance['hasEntryComments']);
         $t->same(3, $provenance['entryCommentCount']);
         $t->same(['META-INF/manifest.xml', 'content.xml', 'Pictures/hero.png'], $provenance['commentedEntryNames']);
+        $zipPackageManifestEntryCommentFields = [
+            'hasEntryComments' => 'zipPackageManifestHasEntryComments',
+            'commentedEntryNames' => 'zipPackageManifestCommentedEntryNames',
+            'entryCommentSummaryCount' => 'zipPackageManifestEntryCommentSummaryCount',
+            'entryCommentSourceRecordBytes' => 'zipPackageManifestEntryCommentSourceRecordBytes',
+            'entryCommentSummaries' => 'zipPackageManifestEntryCommentSummaries',
+        ];
+        foreach ($zipPackageManifestEntryCommentFields as $manifestKey => $surfaceKey) {
+            $t->same($packageManifest[$manifestKey], $provenance[$surfaceKey], "{$surfaceKey} provenance");
+            $t->same($packageManifest[$manifestKey], $identity[$surfaceKey], "{$surfaceKey} identity");
+            $t->same($packageManifest[$manifestKey], $documentProvenance[$surfaceKey], "{$surfaceKey} document provenance");
+        }
+        $t->same(3, $provenance['zipPackageManifestEntryCommentSummaryCount']);
+        $t->same(['META-INF/manifest.xml', 'content.xml', 'Pictures/hero.png'], $documentProvenance['zipPackageManifestCommentedEntryNames']);
 
         $t->same('body review', $content['zipEntryComment']);
         $t->same(strlen('body review'), $content['zipEntryCommentLength']);
