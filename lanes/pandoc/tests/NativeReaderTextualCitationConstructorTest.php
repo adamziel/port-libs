ad25cefeeb5336abf053a09bad29a8f050eb56be
Adam Zieliński <adam@adamziel.com>
2026-07-02 00:22:57 +0000

    pandoc: preserve textual native cite sidecars
    Executed-By: port_libs/polecats/1768

--- lanes/pandoc/tests/NativeReaderTextualCitationConstructorTest.php
+++ lanes/pandoc/tests/NativeReaderTextualCitationConstructorTest.php
@@ -45,6 +45,59 @@ return [
   ]
 ]
 NATIVE;
+        $firstPrefix = [
+            ['t' => 'Str', 'c' => 'see'],
+        ];
+        $firstSuffix = [
+            ['t' => 'Str', 'c' => 'p.'],
+            ['t' => 'Space'],
+            ['t' => 'Str', 'c' => '7'],
+        ];
+        $clusterDisplay = [
+            ['t' => 'Str', 'c' => '[see'],
+            ['t' => 'Space'],
+            ['t' => 'Str', 'c' => '@smith1899,'],
+            ['t' => 'Space'],
+            ['t' => 'Str', 'c' => 'p.'],
+            ['t' => 'Space'],
+            ['t' => 'Str', 'c' => '7;'],
+            ['t' => 'Space'],
+            ['t' => 'Str', 'c' => '@doe1901]'],
+        ];
+        $singleSuffix = [
+            ['t' => 'Str', 'c' => 'appendix'],
+        ];
+        $singleDisplay = [
+            ['t' => 'Str', 'c' => '-@anonymous2026,'],
+            ['t' => 'Space'],
+            ['t' => 'Str', 'c' => 'appendix'],
+        ];
+        $firstRecord = [
+            'citationId' => 'smith1899',
+            'citationPrefix' => $firstPrefix,
+            'citationSuffix' => $firstSuffix,
+            'citationMode' => ['t' => 'NormalCitation'],
+            'citationNoteNum' => 3,
+            'citationHash' => 1899,
+        ];
+        $secondRecord = [
+            'citationId' => 'doe1901',
+            'citationPrefix' => [],
+            'citationSuffix' => [],
+            'citationMode' => ['t' => 'AuthorInText'],
+            'citationNoteNum' => 4,
+            'citationHash' => 1901,
+        ];
+        $singleRecord = [
+            'citationId' => 'anonymous2026',
+            'citationPrefix' => [],
+            'citationSuffix' => $singleSuffix,
+            'citationMode' => ['t' => 'SuppressAuthor'],
+            'citationNoteNum' => 5,
+            'citationHash' => 2026,
+        ];
+        $clusterNative = ['t' => 'Cite', 'c' => [[$firstRecord, $secondRecord], $clusterDisplay]];
+        $singleNative = ['t' => 'Cite', 'c' => [[$singleRecord], $singleDisplay]];
 
         $nativeDocument = (new NativeReader())->read($nativeText);
         $document = new AstNode('document', [
@@ -59,14 +112,24 @@ NATIVE;
         $roundTrip = (new PandocJsonReader())->readPacket($jsonPacket);
 
         $t->same('citation_group', $cluster->type);
+        $t->same('Cite', $cluster->attr('constructor'));
+        $t->same($clusterNative, $cluster->attr('native'));
+        $t->same([$firstRecord, $secondRecord], $cluster->attr('citationRecordsNative'));
         $t->same('[see @smith1899, p. 7; @doe1901]', $cluster->attr('text'));
         $t->same(['smith1899', 'doe1901'], array_map(static fn (AstNode $node): string => $node->attr('id'), $cluster->children));
+        $t->same('Citation', $cluster->children[0]->attr('citationConstructor'));
+        $t->same($firstRecord, $cluster->children[0]->attr('citationNative'));
+        $t->same($secondRecord, $cluster->children[1]->attr('citationNative'));
         $t->same('see', $cluster->children[0]->attr('prefix')[0]->attr('text'));
         $t->same('p. 7', $cluster->children[0]->attr('suffix')[0]->attr('text') . ' ' . $cluster->children[0]->attr('suffix')[2]->attr('text'));
         $t->same('author_in_text', $cluster->children[1]->attr('mode'));
         $t->same(3, $cluster->children[0]->attr('citationNoteNum'));
         $t->same(1901, $cluster->children[1]->attr('citationHash'));
         $t->same('citation', $singleCitation->type);
+        $t->same('Cite', $singleCitation->attr('constructor'));
+        $t->same($singleNative, $singleCitation->attr('native'));
+        $t->same([$singleRecord], $singleCitation->attr('citationRecordsNative'));
+        $t->same($singleRecord, $singleCitation->attr('citationNative'));
         $t->same('anonymous2026', $singleCitation->attr('id'));
         $t->same('suppress_author', $singleCitation->attr('mode'));
         $t->same('-@anonymous2026, appendix', $singleCitation->attr('text'));
@@ -89,6 +152,25 @@ NATIVE;
         $t->same('citation_group', $roundTrip->children[0]->children[2]->type);
         $t->same('citation', $roundTrip->children[0]->children[4]->type);
         $t->same('anonymous2026', $roundTrip->children[0]->children[4]->attr('id'));
+
+        $editedCluster = new AstNode('citation_group', $cluster->attrs, [
+            new AstNode('citation', array_replace($cluster->children[0]->attrs, ['citationHash' => 2000, 'hash' => 2000]), $cluster->children[0]->children),
+            $cluster->children[1],
+        ]);
+        $edited = new AstNode('document', $document->attrs, [
+            new AstNode('paragraph', [], [$editedCluster]),
+        ]);
+        foreach ([
+            'json' => (new PandocJsonWriter())->toArray($edited),
+            'native' => json_decode((new NativeWriter())->write($edited), true, 512, JSON_THROW_ON_ERROR),
+        ] as $writer => $editedPacket) {
+            $editedCite = $editedPacket['blocks'][0]['c'][0];
+
+            $t->same('Cite', $editedCite['t'], "{$writer} regenerates edited Cite constructor");
+            $t->same(2000, $editedCite['c'][0][0]['citationHash'], "{$writer} updates edited citation hash");
+            $t->same(1901, $editedCite['c'][0][1]['citationHash'], "{$writer} keeps untouched citation hash");
+            $t->same($clusterDisplay, $editedCite['c'][1], "{$writer} preserves Cite display inlines");
+        }
     },
     'rejects malformed textual native cite constructors before writer handoff' => static function (TestRunner $t): void {
         $reader = new NativeReader();
